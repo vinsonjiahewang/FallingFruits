29eb324ee2a6693ba50f5f13e7b810ff37833bfd
Vinson Wang <32784164+vinsonjiahewang@users.noreply.github.com>
2017-10-15 00:28:01 -0400

    Add files via upload

--- FallingFruits/Classes/Player Class/Player.swift
+++ FallingFruits/Classes/Player Class/Player.swift
@@ -1,1 +1,49 @@
+//
+//  Player.swift
+//  FallingFruits
+//
+//  Created by Ayush Jaiswal on 10/14/17.
+//  Copyright © 2017 Ayush&Vinson. All rights reserved.
+//
+
+
+import SpriteKit
+
+class Player: SKSpriteNode {
+    
+    private var minX = CGFloat(-200), maxX = CGFloat(200);
+    
+    
+    //This function will give info to the controller about the collison
+    func initializePlayer() {
+        name = "Player";
+        
+        physicsBody = SKPhysicsBody(circleOfRadius: size.height / 2);
+        physicsBody?.affectedByGravity = false;
+        physicsBody?.isDynamic = false;
+        physicsBody?.categoryBitMask = ColliderType.PLAYER;
+        physicsBody?.contactTestBitMask = ColliderType.FRUIT_AND_BOMB;
+        
+    }
+    
+    func move(left: Bool) {
+        
+        if left {
+            
+            position.x -= 15;
+            
+            if position.x < minX {
+                position.x = minX;
+            }
+            
+        } else {
+            position.x += 15;
+            
+            if position.x > maxX {
+                position.x = maxX;
+            }
+        }
+    }
+    
+}//class
 
